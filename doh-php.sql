2c65312250bbb68cda2983e6d1b5a65d51638a5a
hugangba <47004704+hugangba@users.noreply.github.com>
2025-11-30 02:15:49 +0800

    Update doh-php.sql

--- doh-php.sql
+++ doh-php.sql
@@ -1,65 +1,18 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE dns_id_counter_v4 (id INT UNSIGNED NOT NULL DEFAULT 0) ENGINE=InnoDB;
+CREATE TABLE dns_id_counter_v6 (id INT UNSIGNED NOT NULL DEFAULT 0) ENGINE=InnoDB;
+INSERT INTO dns_id_counter_v4 VALUES (0);
+INSERT INTO dns_id_counter_v6 VALUES (0);
 
--- --------------------------------------------------------
+CREATE TABLE dns_records_ipv4 (
+    id        INT UNSIGNED NOT NULL PRIMARY KEY,
+    domain    VARCHAR(255) NOT NULL UNIQUE KEY,
+    ipv4      VARCHAR(15) NOT NULL,
+    timestamp INT UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- 表的结构 `dns_records_ipv4`
---
-
-CREATE TABLE `dns_records_ipv4` (
-  `id` int NOT NULL,
-  `domain` varchar(255) NOT NULL,
-  `ipv4` varchar(15) NOT NULL,
-  `timestamp` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `dns_records_ipv6`
---
-
-CREATE TABLE `dns_records_ipv6` (
-  `id` int NOT NULL,
-  `domain` varchar(255) NOT NULL,
-  `ipv6` varchar(39) NOT NULL,
-  `timestamp` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
---
--- 转储表的索引
---
-
---
--- 表的索引 `dns_records_ipv4`
---
-ALTER TABLE `dns_records_ipv4`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `idx_domain` (`domain`);
-
---
--- 表的索引 `dns_records_ipv6`
---
-ALTER TABLE `dns_records_ipv6`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `idx_domain` (`domain`);
-
---
--- 在导出的表使用AUTO_INCREMENT
---
-
---
--- 使用表AUTO_INCREMENT `dns_records_ipv4`
---
-ALTER TABLE `dns_records_ipv4`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- 使用表AUTO_INCREMENT `dns_records_ipv6`
---
-ALTER TABLE `dns_records_ipv6`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+CREATE TABLE dns_records_ipv6 (
+    id        INT UNSIGNED NOT NULL PRIMARY KEY,
+    domain    VARCHAR(255) NOT NULL UNIQUE KEY,
+    ipv6      VARCHAR(39) NOT NULL,
+    timestamp INT UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
